28395ff13fa811dc0024bc6fe007e01e7244ed92
Jiaxi Hu <jiaxihu@gmail.com>
2014-03-22 23:35:46 -0400

    whatever, texture is working, working on lighting

--- android/assets/shader/simplefrag.glsl
+++ android/assets/shader/simplefrag.glsl
@@ -1,4 +1,5 @@
 precision lowp float;
+uniform sampler2D u_TextureUnit;
 varying vec2 v_TextureCoordinates;
 varying vec3 v_Normal;
 varying vec4 debugColor;
@@ -8,6 +9,7 @@ void main() {
     //vec4 DiffuseColor = debugColor * 3.0f; 
     //DiffuseColor = vec4(1.0f, 1.0f, 1.0f, 1.0f) * DiffuseFactor;
     
-	gl_FragColor = debugColor;
-	//gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);  
+	//gl_FragColor = debugColor;
+	//gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0); 
+	gl_FragColor = texture2D(u_TextureUnit, v_TextureCoordinates); 
 }
