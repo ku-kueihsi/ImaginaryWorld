6506ee04874ba9ed2fc2dfb48e2976ac05e7229a
Jiaxi Hu <jiaxihu@gmail.com>
2014-03-22 23:47:39 -0400

    lighting done

--- android/assets/shader/simplefrag.glsl
+++ android/assets/shader/simplefrag.glsl
@@ -1,15 +1,51 @@
+//#define IW_GL_ES 1
+
+//#if IW_GL_ES
+ 
+////OpenGL ES 2.0
+//#version 100
+//#extension GL_EXT_separate_shader_objects : enable
+//#extension GL_ARB_separate_shader_objects : enable
+//precision mediump float;
 precision lowp float;
+#define IN varying
+
+//#else //PC GL 330+
+
+//#version 330
+//#define IN in
+//#define lowp
+//#define mediump
+//#define highp
+
+//#endif
+
 uniform sampler2D u_TextureUnit;
-varying vec2 v_TextureCoordinates;
-varying vec3 v_Normal;
-varying vec4 debugColor;
-void main() {
-	//u_TextureUnit = 0;
-    //float DiffuseFactor = dot(normalize(v_Normal), normalize(vec3(1.0f, -1.0f, 1.0f)));
-    //vec4 DiffuseColor = debugColor * 3.0f; 
-    //DiffuseColor = vec4(1.0f, 1.0f, 1.0f, 1.0f) * DiffuseFactor;
-    
-	//gl_FragColor = debugColor;
-	//gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0); 
-	gl_FragColor = texture2D(u_TextureUnit, v_TextureCoordinates); 
+IN vec2 v_TextureCoordinates;
+IN vec3 v_Normal;
+IN vec4 debugColor;
+
+void main()
+{
+    //u_TextureUnit = 0;
+    float DiffuseFactor = dot(normalize(v_Normal), normalize(vec3(1.0, -1.0, 1.0)));
+    vec4 DiffuseColor;                                                              
+                                                                                    
+//    if (DiffuseFactor > 0) {                                                        
+        DiffuseColor = vec4(1.0, 1.0, 1.0, 1.0) * DiffuseFactor;                                               
+//    }                                                                               
+//    else {                                                                          
+//        DiffuseColor = vec4(0.0, 0.0, 0.0, 0.0);                                            
+//    } 
+    gl_FragColor = texture2D(u_TextureUnit, v_TextureCoordinates) * (DiffuseColor + vec4(0.1, 0.1, 0.1, 0.0));
+//    gl_FragColor = debugColor;
+//    gl_FragColor = vec4(1,1,1,1);
+
+    //gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
 }
+
+//varying vec4 v_color;
+//void main() {
+//	gl_FragColor = v_color;
+//	//gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);  
+//}
